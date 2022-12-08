a142cd8d5670e66fb7e8c486557921724057b088
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2022-12-08 11:51:02 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,5 @@
 #!/bin/bash
 #include <stdio.h>
-
 /**
  *
  *  * main - Entry point
@@ -14,6 +13,6 @@
 int main(void)
 
 {
-	        printf("with proper grammar, but the outcome is a piece of art,\n");
-		        return (0);
+ 	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
