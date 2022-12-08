bdb2a4880a4c95e7e70271941f10e41c7a5f865d
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2022-12-08 10:32:47 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,18 @@
 #!/bin/bash
-int puts "Programming is like building a multilingual puzzle"
+#include <studio.h>
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{	puts("/"Programming is like building a multilingual puzzle");
+	 return (0);
+}
+
