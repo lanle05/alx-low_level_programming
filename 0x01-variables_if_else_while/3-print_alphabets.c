5033eb459d20223d56da2d0d12c7728c087905c9
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2022-12-09 06:53:37 -0800

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+	char r;
+	
+	for(r = 'a'; r <= 'z'; r++)
+	{
+		putchar(r)
+	}
+	for (r = 'A'; r <= 'Z'; r++)
+	{
+		putchar(r)
+	}
+	putchar('\n')
+	return (0);
+
+}
