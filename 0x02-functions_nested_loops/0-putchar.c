dd08fc978ddd5c360b37ffdd9d43bcf1df4cf5f1
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2022-12-14 04:00:06 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-  char c[] = "_putchar";
-  int i;
+	char c[] = "_putchar";
+  	int i;
 
-  for (i = 0; i < 8; i++)
-    {
-      _putchar(c[i]);
-    }
-  _putchar('\n');
-  return (0);
+  	for (i = 0; i < 8; i++)
+	{
+		_putchar(c[i])
+	}
+	_putchar('\n');
+	return (0);
 }
