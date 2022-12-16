2206fe9737842c3e76046b27c507a1f78fa3ceb9
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2022-12-16 01:10:37 -0800

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,18 @@
+#include "main.h"
+
+
+
+/**
+ * main - check the code for Holberton School students.
+ *  
+ * Return: Always 0.
+*/
+
 void print_alphabet(void);
+
+{
+	print_alphabet();
+	
+	return (0);
+
+}
