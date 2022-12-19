48cbb888f372cdc8c8abb1981ca90f80d726c463
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2022-12-19 05:19:29 -0800

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/** 
+ * int _strlen - returns the length of a string
+ * @s - the string
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int i;
+       
+	i = 0;
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	return(i);
+}
+
