32139426839f2f12e759651e0546acebdb311c45
lanle05 <117745472+lanle05@users.noreply.github.com>
2023-01-12 21:23:10 +0100

    Update 3-strcmp.c

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings.
+ * @s1: first string
+ * @s2: second string
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int diff = 0;
+
+	for (i = 0; s1[i] && s2[i]; i++)
+	{
+
+		if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+		{
+			diff = s1[i] - s2[i];
+		}
+	}
+	return (diff);
+  }
