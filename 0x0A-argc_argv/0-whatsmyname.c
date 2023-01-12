4f58c0240b7bf05d5bdf378e3d7efcad98002fdc
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2023-01-12 13:21:11 -0800

    Arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#main.h
+/*
+ * main: passing argument
+ * @argv:array to pointer
+ * @argc:counter
+ *Return:0
+ */
+
+int main(int __attribute__((unused)) argc, char*argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
