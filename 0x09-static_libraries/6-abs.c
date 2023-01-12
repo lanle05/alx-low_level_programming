5a4a614f9869b54f8b9c9e3cc0cbdcd5dce6c5d5
lanle05 <117745472+lanle05@users.noreply.github.com>
2023-01-12 21:27:50 +0100

    Update 6-abs.c

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - find the absolute value of a certain number
+ * @i: number under test
+ * Return: always 0
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		return (-i);
+	}
+	return (i);
+
+}
