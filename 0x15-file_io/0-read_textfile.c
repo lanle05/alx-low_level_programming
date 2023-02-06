5b1abab417b8c9445cde4abe4196acba91e7230d
lanle05 <117745472+lanle05@users.noreply.github.com>
2023-02-07 00:07:40 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,43 @@
-#include <sys/types.>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t read_count, write_count;
-	char *buffer;
+  int fd;
+  ssize_t read_count, write_count;
+  char *buffer;
+
+  if (!filename) {
+    return 0;
+  }
+
+  fd = open(filename, O_RDONLY);
+  if (fd == -1) {
+    return 0;
+  }
+
+  buffer = malloc(letters);
+  if (!buffer) {
+    close(fd);
+    return 0;
+  }
+
+  read_count = read(fd, buffer, letters);
+  if (read_count == -1) {
+    free(buffer);
+    close(fd);
+    return 0;
+  }
+
+  write_count = write(STDOUT_FILENO, buffer, read_count);
+  free(buffer);
+  close(fd);
+
+  if (write_count != read_count) {
+    return 0;
+  }
+
+  return read_count;
+}
